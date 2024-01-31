f7fc72ed3bf58179cac3f805b770fbbaa0b5b64e
amuvu <10873610@uvu.edu>
2024-01-30 22:37:04 -0700

    finished prepare assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+        Console.Clear();
+
+
+        Assignment first = new("Samuel Bennett", "Multiplication");
+        Console.WriteLine(first.GetSummary());
+
+        MathAssignment math = new("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment write = new("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
     }
 }
